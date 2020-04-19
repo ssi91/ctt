f8af745c513eb01273df244142ac302cee4ba14e
ssi91 <sergeev.s.i1991@gmail.com>
2020-04-19 11:30:00 +0700

    add SendEtherToAddress function

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ssi91/ctt
 
 go 1.13
 
-require github.com/ethereum/go-ethereum v1.9.10
+require github.com/ethereum/go-ethereum v1.9.13
--- keys_creator.go
+++ keys_creator.go
@@ -1,9 +1,12 @@
 package ctt
 
 import (
+	"context"
 	"crypto/ecdsa"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"log"
 	"math/big"
@@ -13,6 +16,13 @@ type AllocatedSimulatedBackend struct {
 	backends.SimulatedBackend
 	allocatedKeys *[]*ecdsa.PrivateKey
 }
+
+//type SmartContract struct {
+//	Contract        *interface{}
+//	ContractAddress common.Address
+//	OwnerKey        *ecdsa.PrivateKey
+//	SystemKey       *ecdsa.PrivateKey
+//}
 
 func NewAllocatedSimulatedBackend(amount uint) *AllocatedSimulatedBackend {
 	alloc, keys := generateKeysAlloc(amount)
@@ -35,6 +45,44 @@ func (b *AllocatedSimulatedBackend) Keys() *[]*ecdsa.PrivateKey {
 		log.Fatal("Backend's Keys are not initialized")
 	}
 	return b.allocatedKeys
+}
+
+func SendEtherToAddress(ctx context.Context, backend *AllocatedSimulatedBackend, senderKey *ecdsa.PrivateKey, receiverAddress *common.Address, value *big.Int) (*types.Receipt, error) {
+	nonce, err := backend.PendingNonceAt(ctx, crypto.PubkeyToAddress(senderKey.PublicKey))
+	if err != nil {
+		log.Printf("SendEtherToAddress error: %s\n", err)
+		return nil, err
+	}
+
+	var gasLimit uint64 = 2100000
+
+	gasPrice, err := backend.SuggestGasPrice(ctx)
+	if err != nil {
+		log.Printf("SendEtherToAddress gas price error: %s\n", err)
+		return nil, err
+	}
+
+	var data []byte
+	tx := types.NewTransaction(nonce, *receiverAddress, value, gasLimit, gasPrice, data)
+
+	chainID := big.NewInt(1337)
+
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), senderKey)
+	if err != nil {
+		log.Printf("SendEtherToAddress sign error: %s\n", err)
+		return nil, err
+	}
+
+	err = backend.SendTransaction(ctx, signedTx)
+	if err != nil {
+		log.Printf("SendEtherToAddress SendTransaction: %s\n", err)
+		return nil, err
+	}
+
+	backend.Commit()
+	//log.Printf("gas costed: %s", signedTx.Data())
+
+	return backend.TransactionReceipt(ctx, signedTx.Hash())
 }
 
 func generateKeysAlloc(amount uint) (*core.GenesisAlloc, *[]*ecdsa.PrivateKey) {
